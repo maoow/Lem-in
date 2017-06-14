5cc0640dcfe9bf8973435ed3244fa5d089551112
Corentin BINET <cbinet@e3r5p10.42.fr>
2017-06-14 14:01:07 +0200

    check already defined tubes

--- srcs/ft_lemgetenv.c
+++ srcs/ft_lemgetenv.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/02 10:29:02 by cbinet            #+#    #+#             */
-/*   Updated: 2017/05/13 15:16:47 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/06/14 13:35:55 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,19 +82,19 @@ void		ft_addtube(t_lemenv *env, char *str)
 	if (j < env->roomsnb)
 	{
 		tmproom = env->rooms[j];
-	free(tmp);
-	tmp = ft_strdup(str + i + 1);
-	j = 0;
-	while (j < env->roomsnb && ft_strcmp(env->rooms[j]->name, tmp))
-		j++;
-	if (j < env->roomsnb)
-	{
-		ft_addneighbors(tmproom, env->rooms[j]);
-	free(tmp);
-	free(str);
-	}
-	else
-		ft_error(env, tmp, " is not a room", false);
+		free(tmp);
+		tmp = ft_strdup(str + i + 1);
+		j = 0;
+		while (j < env->roomsnb && ft_strcmp(env->rooms[j]->name, tmp))
+			j++;
+		if (j < env->roomsnb)
+		{
+			ft_addneighbors(tmproom, env->rooms[j]);
+			free(tmp);
+			free(str);
+		}
+		else
+			ft_error(env, tmp, " is not a room", false);
 	}
 	else
 		ft_error(env, tmp, " is not a room", false);
@@ -113,28 +113,28 @@ void		ft_getlemmap(t_lemenv *env)
 	tube = false;
 	if (get_next_line(0, &str) && ft_strlen(str))
 	{
-	ft_lemkeepmap(env, str);
-	while (str[j] == '0')
-		j++;
-	while (str[i] && ft_isdigit(str[i]))
-		i++;
-	str[i] = '\0';
-	if (i - j > 10 || (i - j  == 10 && ft_strcmp("2147483647", str + j) < 0))
-		ft_error(env, "ant quantity up to int max", "", true);
-			env->ants = ft_atoi(str);
+		ft_lemkeepmap(env, str);
+		while (str[j] == '0')
+			j++;
+		while (str[i] && ft_isdigit(str[i]))
+			i++;
+		str[i] = '\0';
+		if (i - j > 10 || (i - j  == 10 && ft_strcmp("2147483647", str + j) < 0))
+			ft_error(env, "ant quantity up to int max", "", true);
+		env->ants = ft_atoi(str);
 		free(str);
 	}
 	if (env->ants == 0)
-		ft_error(env, "No ants", "", true);
+		ft_error(env, "No ants or negative number of ants", "", true);
 	while (get_next_line(0, &str))
 	{
-	ft_lemkeepmap(env, str);
-		if (ft_strchr(str, '-'))
+		ft_lemkeepmap(env, str);
+		if (ft_strchr(str, '-') && str[0] != '#')
 		{
 			tube = true;
 			ft_addtube(env, str);
 		}
-		else if ((room = ft_getroom(str, env, tube)))
+		else if ((room = ft_getroom(str, env, tube)) && !tube)
 			ft_pushroom(env, room);
 	}
 }
