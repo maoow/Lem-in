41df43ff9954b5359dd4fac6abf2c22a3746f3f4
maoow <cbinet@student.42.fr>
2017-06-17 13:37:27 +0200

    boolean method

--- srcs/ft_checkrooms.c
+++ srcs/ft_checkrooms.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/17 12:29:23 by cbinet            #+#    #+#             */
-/*   Updated: 2017/06/17 12:52:29 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/06/17 13:31:34 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,26 @@ bool	ft_checkuniquename(t_lemenv *env, size_t j)
 {
 	size_t		i;
 
+	i = 0;
 	while (env->rooms[i])
 	{
-		if (!ft_strcmp(env->rooms[i].name, env->rooms[j].name) && i != j)
+		if (!ft_strcmp(env->rooms[i]->name, env->rooms[j]->name) && i != j)
 			return (false);
-		i++
+		i++;
+	}
+	return (true);
+}
+
+bool	ft_checkuniquecoo(t_lemenv *env, size_t j)
+{
+	size_t		i;
+
+	i = 0;
+	while (env->rooms[i])
+	{
+		if (env->rooms[i]->coo.y == env->rooms[j]->coo.y && env->rooms[i]->coo.x == env->rooms[j]->coo.x && i != j)
+			return (false);
+		i++;
 	}
 	return (true);
 }
@@ -28,12 +43,17 @@ bool	ft_checkuniquename(t_lemenv *env, size_t j)
 bool	ft_checkrooms(t_lemenv *env)
 {
 	size_t		i;
+	bool		error;
 
+	error = false;
 	i = 0;
-	while (env->rooms[i])
+	while (env->rooms[i] && !error)
 	{
-		if (!ft_checkuniquename(i))
-			ft_error();
-		i++
+		if ((error = !ft_checkuniquecoo(env, i)))
+			ft_error(env, "not unique coo", "", false);
+		if ((error = !ft_checkuniquename(env, i)))
+			ft_error(env, "not unique name", "", false);
+		i++;
 	}
+	return (!error);
 }
