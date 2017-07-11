b97d2ee844dbd86240cb844e4d14ff53620c66a2
maoow <cbinet@student.42.fr>
2017-07-11 15:18:49 +0200

    adaptation to new ft_error

--- srcs/ft_lemgetrooms.c
+++ srcs/ft_lemgetrooms.c
@@ -6,7 +6,7 @@
 /*   By: cbinet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/02 12:13:09 by cbinet            #+#    #+#             */
-/*   Updated: 2017/07/08 08:59:11 by cbinet           ###   ########.fr       */
+/*   Updated: 2017/07/11 15:13:56 by cbinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ void			ft_getroomcoo(t_lemenv *env, char *str, t_lroom *room)
 	if (str[i])
 		room->coo.y = ft_atoi(str + i);
 	else
-		ft_error(env, "incorrect room name/coordinates", "", true);
+		ft_error(env, "incorrect room name/coordinates", true);
 	room->ants = 0;
 }
 
@@ -62,11 +62,19 @@ t_lroom			*ft_setroom(t_lemenv *env, t_lroom *room, bool start, bool end)
 {
 	if (start)
 	{
+		if (end)
+			ft_error(env, "can't set the same room as start and end", true);
+		if (env->start)
+			ft_error(env, "can't set multiple rooms as start", true);
 		env->start = room;
 		room->ants = env->ants;
 	}
 	if (end)
+	{
+		if (env->end)
+			ft_error(env, "can't set multiple rooms as end", true);
 		env->end = room;
+	}
 	room->last = end;
 	return (room);
 }
@@ -81,16 +89,16 @@ t_lroom			*ft_getroom(char *str, t_lemenv *env, bool tube)
 	end = false;
 	while (str[0] == '#')
 	{
-		start = !ft_strcmp("##start", str);
-		end = !ft_strcmp("##end", str);
-		if (str && ft_strcmp("##start", str) && ft_strcmp("##end", str))
+		start = !ft_strcmp("##start", str) || start;
+		end = !ft_strcmp("##end", str) || end;
+		if (str && !start && !end)
 			return (NULL);
 		free(str);
 		get_next_line(0, &str);
 		ft_lemkeepmap(env, str);
 	}
 	if (tube || str[0] == 'L' || ft_strchr(str, '-'))
-		ft_error(env, "not well formated room/tube", str, true);
+		ft_error(env, "not well formated room/tube", true);
 	else if ((room = (t_lroom*)malloc(sizeof(t_lroom))))
 	{
 		ft_getroomcoo(env, str, room);
